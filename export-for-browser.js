95d17f362df64a8e1e0eb3a57f9a93ad0dc77c9f
Pegep <pekka@sektori.com>
2016-10-08 09:58:30 +0200

    Add home and away teams to data.json

--- export-for-browser.js
+++ export-for-browser.js
@@ -5,33 +5,46 @@ var async = require('async');
 var fs = require('fs');
 var _ = require('underscore');
 
-var dbMatches = new Datastore({
-    filename: 'matches.nedb',
-    autoload: true
-});
+var dbs = {};
+var sections = ['matches', 'pitches', 'groups', 'teams', 'competition_categories'];
 
-var dbPitches = new Datastore({
-    filename: 'pitches.nedb',
-    autoload: true
+sections.forEach((item) => {
+    dbs[item] = new Datastore({
+	filename: item + '.nedb',
+	autoload: true
+    });
 });
 
 var out = [],
     outJson;
 
-dbMatches.find({}, (err, matches) => {
+dbs['matches'].find({}, (err, matches) => {
     async.eachSeries(matches, (match, cb) => {
-	dbPitches.findOne({ id: match.relationships.pitch.data.id }, (err, pitch) => {
-	    var retMatch = {
-		'start_time': match.attributes.start_time,
-		'end_time': match.attributes.end_time,
-		'home_points': match.attributes.home_points,
-		'away_points': match.attributes.away_points,
-		'name': match.attributes.name,
-		'pitch': pitch.attributes.abbreviation
-	    };
+	dbs['pitches'].findOne({ id: match.relationships.pitch.data.id }, (err, pitch) => {
+	    async.parallel([
+		(cb) => {
+		    dbs['teams'].findOne({ id: match.relationships.home_team.data.id }, (err, team) => { cb(null, team); });
+		},
+		(cb) => {
+		    dbs['teams'].findOne({ id: match.relationships.away_team.data.id }, (err, team) => { cb(null, team); });
+		}
+	    ], (err, teams) => {
+		let homeTeam = teams[0];
+		let awayTeam = teams[1];
 
-	    out.push(retMatch);
-	    cb();
+		var retMatch = {
+		    'start_time': match.attributes.start_time,
+		    'end_time': match.attributes.end_time,
+		    'home_points': match.attributes.home_points,
+		    'away_points': match.attributes.away_points,
+		    'pitch': pitch.attributes.abbreviation,
+		    'home_team': homeTeam,
+		    'away_team': awayTeam
+		};
+
+		out.push(retMatch);
+		cb();
+	    });
 	});
     }, (err) => {
 	outJson = 'var data = ' + JSON.stringify(out);
