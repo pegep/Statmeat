000135b6abb391a5fc86c7ad5ecdc9d3ca07baff
pegep <pekka@sektori.com>
2016-10-10 11:05:21 +0300

    Add category to table listing

--- export-for-browser.js
+++ export-for-browser.js
@@ -1,12 +1,12 @@
 "use strict";
 
-var Datastore = require('nedb');
-var async = require('async');
-var fs = require('fs');
-var _ = require('underscore');
+let Datastore = require('nedb');
+let async = require('async');
+let fs = require('fs');
+let _ = require('underscore');
 
-var dbs = {};
-var sections = ['matches', 'pitches', 'groups', 'teams', 'competition_categories'];
+let dbs = {};
+let sections = ['matches', 'pitches', 'groups', 'teams', 'competition_categories'];
 
 sections.forEach((item) => {
     dbs[item] = new Datastore({
@@ -15,30 +15,51 @@ sections.forEach((item) => {
     });
 });
 
-var out = [],
+let out = [],
     outJson;
 
-dbs['matches'].find({}, (err, matches) => {
+new Promise((resolve, rejecct) => {
+    dbs['matches'].find({}, (err, matches) => {
+	resolve(matches);
+    });
+}).then((matches) => {
     async.eachSeries(matches, (match, cb) => {
-	dbs['pitches'].findOne({ id: match.relationships.pitch.data.id }, (err, pitch) => {
-	    async.parallel([
-		(cb) => {
-		    dbs['teams'].findOne({ id: match.relationships.home_team.data.id }, (err, team) => { cb(null, team); });
-		},
-		(cb) => {
-		    dbs['teams'].findOne({ id: match.relationships.away_team.data.id }, (err, team) => { cb(null, team); });
-		}
-	    ], (err, teams) => {
-		let homeTeam = teams[0];
-		let awayTeam = teams[1];
+	let homeTeamP = new Promise((resolve, reject) => {
+	    dbs['teams'].findOne({ id: match.relationships.home_team.data.id }, (err, team) => {
+		resolve(team);
+	    });
+	});
+	let awayTeamP = new Promise((resolve, reject) => {
+	    dbs['teams'].findOne({ id: match.relationships.away_team.data.id }, (err, team) => {
+		resolve(team);
+	    });
+	});
+	let pitchP = new Promise((resolve, reject) => {
+	    dbs['pitches'].findOne({ id: match.relationships.pitch.data.id }, (err, pitch) => {
+		resolve(pitch);
+	    });
+	});
+	let categoryP = new Promise((resolve, reject) => {
+	    dbs['competition_categories'].findOne({ id: match.relationships.category.data.id }, (err, category) => {
+		resolve(category);
+	    });
+	});
 
-		var retMatch = {
+	Promise.all([homeTeamP, awayTeamP, pitchP, categoryP])
+	    .then((result) => {
+		let homeTeam = result[0];
+		let awayTeam = result[1];
+		let pitch = result[2];
+		let category = result[3];
+
+		let retMatch = {
 		    'start_time': match.attributes.start_time,
 		    'end_time': match.attributes.end_time,
 		    'home_points': match.attributes.home_points,
 		    'away_points': match.attributes.away_points,
 		    'name': match.attributes.name,
-		    'pitch': pitch.attributes.abbreviation,
+		    'pitch_abbreviation': pitch.attributes.abbreviation,
+		    'category_name': category.attributes.name,
 		    'home_team_name': homeTeam.attributes.name,
 		    'home_team_picture_small': homeTeam.attributes.picture.small,
 		    'home_team_hometown': homeTeam.attributes.hometown,
@@ -48,9 +69,9 @@ dbs['matches'].find({}, (err, matches) => {
 		};
 
 		out.push(retMatch);
+
 		cb();
 	    });
-	});
     }, (err) => {
 	outJson = 'var data = ' + JSON.stringify(out);
 	fs.writeFile('data.json', outJson, 'utf8');
--- parser.js
+++ parser.js
@@ -7,40 +7,49 @@ var _ = require('underscore');
 
 var data = require('./sample-2016-10-11.json');
 
-var matches = data.data
-    .filter((item) => item.type == 'matches')
-    .map((item) => ({ [item.id]: item }));
+var dbs = {};
+var sections = ['matches', 'pitches', 'groups', 'teams', 'competition_categories'];
 
-var pitches = data.included
-    .filter((item) => item.type == 'pitches')
-    .map((item) => ({ [item.id]: item }));
-
-var groups = data.included
-    .filter((item) => item.type == 'groups');
-//    .map((item) => ({ [item.id]: item }));
-
-var teams = data.included
-    .filter((item) => item.type == 'teams')
-    .map((item) => ({ [item.id]: item }));
-
-var competitionCategories = data.included
-    .filter((item) => item.type == 'competition_categories')
-    .map((item) => ({ [item.id]: item }));
-
-console.log(groups);
-
-var db = new Datastore({
-    filename: 'statmeat.nedb',
-    autoload: true
-});
-
-/*
-db.find({}, (err, docs) => {
-    docs.forEach((doc) => {
-	out.push(doc);
+sections.forEach((item) => {
+    dbs[item] = new Datastore({
+	filename: item + '.nedb',
+	autoload: true
     });
-    
-    outJson = 'var data = ' + JSON.stringify(out);
-    fs.writeFile('data.json', outJson, 'utf8');
 });
-*/
+
+dbs['competition_categories'].find({}, (err, docs) => {
+//    console.log(docs);
+});
+
+new Promise((resolve, rejecct) => {
+    dbs['matches'].find({}, (err, matches) => {
+	resolve(matches);
+    });
+}).then((matches) => {
+    async.eachSeries(matches, (match, cb) => {
+	var homeTeamP = new Promise((resolve, reject) => {
+	    dbs['teams'].findOne({ id: match.relationships.home_team.data.id }, (err, team) => {
+		resolve(team);
+	    });
+	});
+	var awayTeamP = new Promise((resolve, reject) => {
+	    dbs['teams'].findOne({ id: match.relationships.away_team.data.id }, (err, team) => {
+		resolve(team);
+	    });
+	});
+	var pitchesP = new Promise((resolve, reject) => {
+	    dbs['pitches'].findOne({ id: match.relationships.pitch.data.id }, (err, pitch) => {
+		resolve(pitch);
+	    });
+	});
+
+	Promise.all([homeTeamP, awayTeamP, pitchesP])
+	    .then((result) => {
+		var homeTeam = result[0];
+		var awayTeam = result[1];
+		var pitch = result[2];
+		cb();
+	    });
+    });
+});
+
