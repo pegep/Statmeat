654cb0f41344d8659246c8b8ebd6360da46c5b6c
Pegep <pekka@sektori.com>
2016-10-13 05:17:12 +0200

    More fix to update-website.sh to make it work with crontab

--- update-website.sh
+++ update-website.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 source "$HOME/.nvm/nvm.sh"
-nvm use node
 
 echo "Removing existing NeDB databases, *.nedb"
 cd "$(dirname "$0")"
 rm -f *.nedb
 
-node crawl.js
-node export-for-browser.js
+$HOME/.nvm/versions/node/v5.0.0/bin/node crawl.js
+$HOME/.nvm/versions/node/v5.0.0/bin/node export-for-browser.js
 
 cp data.json public/
 
+echo "Deploying to Firebase..."
 firebase deploy
