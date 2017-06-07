4228b1fb6cee3f8799877ef66a1e1edf28a583c8
pegep <pekka@sektori.com>
2017-06-07 11:29:37 +0300

    Update update-website.sh to handle different node and Firebase versions

--- update-website.sh
+++ update-website.sh
@@ -6,8 +6,11 @@ echo "Removing existing NeDB databases, *.nedb"
 cd "$(dirname "$0")"
 rm -f *.nedb
 
-$HOME/.nvm/versions/node/v5.0.0/bin/node crawl.js
-$HOME/.nvm/versions/node/v5.0.0/bin/node export-for-browser.js
+NODE_VER=$(nvm current)
+FIREBASE_BIN=$(which firebase)
+
+$HOME/.nvm/versions/node/$NODE_VER/bin/node crawl.js
+$HOME/.nvm/versions/node/$NODE_VER/bin/node export-for-browser.js
 
 DATE=`date +%Y-%m-%d:%H:%M:%S`
 
@@ -16,4 +19,4 @@ sed 's/sr-only//g' public/index.html.template > public/index.html
 sed -i "s/{{last-updated}}/$DATE/g" public/index.html
 
 echo "Deploying to Firebase..."
-$HOME/.nvm/versions/node/v5.0.0/bin/node /usr/local/bin/firebase deploy
+$HOME/.nvm/versions/node/$NODE_VER/bin/node $FIREBASE_BIN deploy
