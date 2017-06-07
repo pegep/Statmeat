151cf3290b5537a80927dacbb7b0226fcd5594d7
pegep <pekka@sektori.com>
2017-06-07 11:24:08 +0300

    Add index.html.template to ease variables in the template Update update-website.sh to replace {{last-updated}} variable with the actual date Update title to be more SEO woop woop

--- update-website.sh
+++ update-website.sh
@@ -9,7 +9,11 @@ rm -f *.nedb
 $HOME/.nvm/versions/node/v5.0.0/bin/node crawl.js
 $HOME/.nvm/versions/node/v5.0.0/bin/node export-for-browser.js
 
+DATE=`date +%Y-%m-%d:%H:%M:%S`
+
 cp data.json public/
+sed 's/sr-only//g' public/index.html.template > public/index.html
+sed -i "s/{{last-updated}}/$DATE/g" public/index.html
 
 echo "Deploying to Firebase..."
 $HOME/.nvm/versions/node/v5.0.0/bin/node /usr/local/bin/firebase deploy
