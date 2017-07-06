0dc945a80fa2fb96b02dfc902809b546c1108708
Pegep <pekka@sektori.com>
2017-07-06 11:55:30 +0200

    Ffffffff.... attempt fixing cron finding the binaries instead of update-website.sh

--- update-website.sh
+++ update-website.sh
@@ -4,9 +4,6 @@ SCRIPT_DIR="$(dirname "$0")"
 echo "Using dir $SCRIPT_DIR"
 cd $SCRIPT_DIR
 
-# add firebase and node binaries to path
-export PATH=$PATH:/usr/local/bin:$HOME/.nvm/versions/node/v8.0.0/bin/node
-
 echo "Removing existing NeDB databases, *.nedb"
 
 rm -f *.nedb
