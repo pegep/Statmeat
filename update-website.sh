801a98fd2456832e98bb363d750427d37a2d66ca
Pegep <pekka@sektori.com>
2017-07-06 11:48:26 +0200

    Even more fixes for update-website.sh to fix finding firebase and node binaries

--- update-website.sh
+++ update-website.sh
@@ -4,12 +4,12 @@ SCRIPT_DIR="$(dirname "$0")"
 echo "Using dir $SCRIPT_DIR"
 cd $SCRIPT_DIR
 
+# add firebase and node binaries to path
+export PATH=$PATH:/usr/local/bin:$HOME/.nvm/versions/node/v8.0.0/bin/node
+
 echo "Removing existing NeDB databases, *.nedb"
 
 rm -f *.nedb
-
-NODE_BIN=$HOME/.nvm/versions/node/$NODE_VER/bin/node
-FIREBASE_BIN=/usr/local/bin/firebase
 
 $NODE_BIN crawl.js
 $NODE_BIN export-for-browser.js
@@ -21,4 +21,4 @@ sed 's/sr-only//g' public/index.html.template > public/index.html
 sed -i "s/{{last-updated}}/$DATE/g" public/index.html
 
 echo "Deploying to Firebase..."
-$FIREBASE_BIN deploy
+firebase deploy
