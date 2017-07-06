99be6d374b64ef875a680774864cccd5ad2d71c2
Pegep <pekka@sektori.com>
2017-07-06 12:37:01 +0200

    Fix cron, fix update-website.sh still... gaah ffs

--- update-website.sh
+++ update-website.sh
@@ -1,15 +1,29 @@
 #!/bin/sh
 
+echo $(date)
+echo
+
 SCRIPT_DIR="$(dirname "$0")"
 echo "Using dir $SCRIPT_DIR"
 cd $SCRIPT_DIR
 
 echo "Removing existing NeDB databases, *.nedb"
 
+NODE_BIN=$(which node)
+FIREBASE_BIN=$(which firebase)
+
+echo "Using node $NODE_BIN"
+echo "Using firebase $FIREBASE_BIN"
+
 rm -f *.nedb
 
-node crawl.js
-node export-for-browser.js
+CRAWL_OUT=$(node crawl.js)
+echo "$CRAWL_OUT"
+echo
+
+EXPORT_OUT=$(node export-for-browser.js)
+echo "$EXPORT_OUT"
+echo
 
 DATE=`date +%Y-%m-%d:%H:%M:%S`
 
@@ -18,4 +32,5 @@ sed 's/sr-only//g' public/index.html.template > public/index.html
 sed -i "s/{{last-updated}}/$DATE/g" public/index.html
 
 echo "Deploying to Firebase..."
-firebase deploy
+DEPLOY_OUT=$(firebase deploy)
+echo "$DEPLOY_OUT"
